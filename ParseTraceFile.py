83d7aaf9b8e2f275a4d8476351b4ef031e7df4e2
lypch <lypch@126.com>
2019-08-13 12:40:02 +0800

    增加注释和输出结果修改
    1. 增加注释
    2. 再结果输出中增加SQL语句

--- ParseTraceFile.py
+++ ParseTraceFile.py
@@ -14,33 +14,36 @@ with open('gcsdata_ora_8396.trc', 'r') as f:
     while len(content) > 0:
         count += 1
 
+        # At first the variable SQLTEXT is none, before encountering the PARSING IN CURSOR.
         if SQLText is not None:
+            # When meet the string 'END OF STMT', means that the SQL statement is complete.
             obj = re.match('END OF STMT', content)
             if obj:
-                sqldict[SQLCursor] = {"sqlid": SQLID, "SQLTEXT":SQLText.replace("\n"," ")}
+                sqldict[SQLID] =  {"sqlid": SQLID, "cursor": SQLCursor, "SQLTEXT":SQLText.replace("\n"," ")}
                 SQLText = None
             else:
                 SQLText += content
         else:
+            # After meet the PARSING IN CURSOR, means that next line is the start of the SQL.
             obj = re.match("PARSING IN CURSOR #([0-9]*)?.*sqlid='(.*)'",content)
             if obj:
                 SQLCursor = obj.group(1)
                 SQLID = obj.group(2)
                 SQLText = ""
             else:
-                obj = re.match('PARSE #([0-9]*)?.*tim=([0-9]*)?.*',content)
+                obj = re.match('PARSE #([0-9]*)?.*mis=([0-9]*)?.*tim=([0-9]*)?.*',content)
                 if obj:
                     cursorNo = obj.group(1)
-                    StartTime = obj.group(2)
-                    datadict[cursorNo] = StartTime
-
+                    sqlmis = obj.group(2)
+                    StartTime = obj.group(3)
+                    datadict[cursorNo] = (sqlmis,StartTime)
                 else:
                     obj = re.match("CLOSE #([0-9]*)?.*tim=([0-9]*)?.*",content)
 
                     if obj:
                         cursorNo = obj.group(1)
                         if cursorNo in datadict:
-                            result.append({"cursor":cursorNo, "start":datadict[cursorNo], "end": obj.group(2), "second": (int(obj.group(2)) - int(datadict[cursorNo]))/1000000 })
+                            result.append({"cursor":cursorNo, "start":datadict[cursorNo][1], "end": obj.group(2), "second": (int(obj.group(2)) - int(datadict[cursorNo][1]))/1000000,"sqlmis": datadict[cursorNo][0]})
                             datadict.pop(cursorNo)
 
         if count % 10000 == 0:
